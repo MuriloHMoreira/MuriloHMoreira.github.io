103eff3aa679641a18128decb43f22fa0a02f75c
Murilo Henrique Moreira <moreira.murilo@gmail.com>
2024-03-04 17:50:16 -0300

    compmatmap

---
+++ scripts/gliderLoad.js
@@ -0,0 +1,43 @@
+document.addEventListener('keydown', (function(e) {
+if (e.keyCode === 37) {
+ // Previous
+ // console.log("prev");
+ window._.scrollItem("prev")
+}
+if (e.keyCode === 39) {
+ // Next
+ // console.log("next");
+ window._.scrollItem("next")
+}
+}));
+
+
+window.addEventListener('load',function(){
+  // document.querySelector('.glider').addEventListener('glider-slide-visible', function(event){
+  //     var glider = Glider(this);
+  //     console.log('Slide Visible %s', event.detail.slide)
+  // });
+  // document.querySelector('.glider').addEventListener('glider-slide-hidden', function(event){
+  //     console.log('Slide Hidden %s', event.detail.slide)
+  // });
+  // document.querySelector('.glider').addEventListener('glider-refresh', function(event){
+  //     console.log('Refresh')
+  // });
+  // document.querySelector('.glider').addEventListener('glider-loaded', function(event){
+  //     console.log('Loaded')
+  // });
+
+  window._ = new Glider(document.querySelector('.glider'), {
+      slidesToShow: 1, //'auto',
+      itemWidth: 800,
+      draggable: false,
+      scrollLock: false,
+      dots: '#dots',
+      rewind: false,
+      arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+      },
+
+  });
+});
---
+++ scripts/tsParticlesScript.js
@@ -0,0 +1,88 @@
+  particlesDensity = 300 / (1710 * 1017);
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  var A = w * h
+
+  tsParticles.load({
+  id: "tsparticles",
+  options: {
+    fullScreen: { enable: false },
+    fpsLimit: 90,
+    particles: {
+      number: {
+        value: particlesDensity * A,
+      },
+      color: {
+        value: "#1e90ff",
+      },
+      shape: {
+        type: "circle"
+      },
+      opacity: {
+        value: 0.5
+      },
+      size: {
+        value: { min: 1, max: 3 }
+      },
+      links: {
+        enable: true,
+        distance: 80,
+        color: "grey",
+        opacity: 1,
+        width: 1,
+        triangles: {
+          enable: true,
+          color: "#ffffff",
+          opacity: 0.1
+        }
+      },
+      move: {
+        enable: true,
+        speed: 0.75,
+        direction: "none",
+        outModes: "bounce"
+      }
+    },
+    interactivity: {
+        detectsOn: "canvas",
+      events: {
+        onHover: {
+          enable: true,
+          mode: "connect"
+        },
+        onClick: {
+            enable: true,
+            mode: "push"
+        },
+        onDiv: [
+        {
+          enable: true,
+          selectors: "#bubble-circle-div",
+          mode: "bounce",
+          type: "rectangle"
+        },
+        ]
+      },
+      modes: {
+        connect: {
+          distance: 200,
+          radius: 80,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+        attract:{
+            distance: 200, 
+            duration: 0.4,
+            factor: 3,
+        },
+      }
+    },
+    background: {
+      color: "#000035"
+    },
+
+
+  }
+});
