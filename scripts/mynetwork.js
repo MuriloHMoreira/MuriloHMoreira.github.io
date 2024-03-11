02bfb52fd08b70b03e40f2e6f4caf976e1d8aa5f
Murilo Henrique Moreira <moreira.murilo@gmail.com>
2024-03-11 15:41:36 -0300

    cmm animation

--- scripts/mynetwork.js
+++ scripts/mynetwork.js
@@ -33,9 +33,7 @@ var data = {
     nodes: nodes,
     edges: edges
 };
-var options = {
-  layout: {improvedLayout: true},
-  groups: {
+var groups = {
     programming: {color:{background: 'grey', border:"black", 
                   hover: {background: "grey", border: "black"},
                   highlight: {background: "grey", border: "black"}},
@@ -57,7 +55,10 @@ var options = {
            borderWidth: 2,
            size: 50,
            shape: 'box'},
-    },
+    }
+var options = {
+  layout: {improvedLayout: true},
+  groups: groups,
   edges: {width: 2,
           arrowStrikethrough: false,
           arrows: {
@@ -74,8 +75,8 @@ physics: false,
 interaction: {
     hover: true,
     dragNodes: true,// do not allow dragging nodes
-    zoomView: true, // do not allow zooming
-    dragView: true,  // do not allow dragging
+    zoomView: false, // do not allow zooming
+    dragView: false,  // do not allow dragging
     multiselect: true,
     navigationButtons: false,
     selectable: true,
@@ -86,9 +87,6 @@ height: "100%"
 };
 var network = new vis.Network(container, data, options);
 
-network.moveTo({
-  scale: 1.5            // Zooms out; 1 is no zoom
-});
 
 // Create info windows
 var allNodes = nodes.get({returnType:"Object"});
@@ -116,6 +114,7 @@ for (nodeId in allNodes){
   divNode.appendChild(divNodeText)
 }
 $("#mynetwork canvas").attr("id","canvas");
+var firstHover = true;
 function showNodeInfo(params) {
       // Get the node ID
       var nodeId = params.node;
@@ -132,27 +131,42 @@ function showNodeInfo(params) {
         var mynetworkCanvas = document.getElementById("canvas");
         var rect = mynetworkCanvas.getBoundingClientRect();
         bb = network.getBoundingBox(nodeId)
-        if(network.getPositions([nodeId])[nodeId].x == 0){
-            nodeX += - 1.8 * bb.right;
-
-        }
-        else{
-            nodeX += rect.left + (bb.right - bb.left)/1.2;
-
-        }
-        nodeY += rect.top - (document.body.getBoundingClientRect().top - (bb.top - bb.bottom));
+        node_width = bb.right - bb.left
+        node_heigth = bb.bottom - bb.top
+        // console.log('Node Id: ', nodeId)
+        // console.log('rect', rect)
+        // console.log('bb', bb)
+        // console.log('nodeX', nodeX)
+        // console.log('nodeY', nodeY)
+        nodeX += + rect.left - 150;
+        nodeY += rect.top - 100 / 2;
         
-        console.log("rank_selection_" + nodeId)
         document.getElementById("node_info_" + nodeId).style.display = "block";
         // Place the div
         document.getElementById("node_info_" + nodeId).style.position = "absolute";
         document.getElementById("node_info_" + nodeId).style.top = nodeY + "px";
         document.getElementById("node_info_" + nodeId).style.left = nodeX + "px";
+        
+        document.getElementById("page-langs").style.background = groups[allNodes[nodeId].group]['color']['background'];
+
+      }
+      if (firstHover){
+        document.getElementById("page-langs").style.transitionProperty += ', background'
       }
 };
 
+ // network.moveTo({'position': {'x': 200, 'y':200}, 'animation': true})
+network.on("zoom", function(){
+})
+
+
+network.on("release", function(){
+  network.fit()
+})
+
 
 function hideNodesInfo(params) {
+document.getElementById("page-langs").style.background = 'slateblue';
 var allNodes = nodes.get({returnType:"Object"});
 var myParent = document.body;
 //Create array of options to be added
@@ -251,4 +265,8 @@ interaction: {
 width: "100%",   // This fills out the canvas size to the encompassing div
 height: "100%"
 };
+
 var network2 = new vis.Network(container2, data2, options2);
+
+
+network.fit()
